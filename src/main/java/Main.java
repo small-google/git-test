29beea9904667e3975345288beed354f19a03a3a
liuxin <394607573@qq.com>
2022-04-15 18:09:01 +0800

    test first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("git test");
         System.out.println("hello git");
-        System.out.println("hell liuxin");
+        System.out.println("hello liuxin");
+        System.out.println("hello world");
     }
 }
