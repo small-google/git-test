2bf115f39b26b846172ab04116342e9231e4a8f0
liuxin <394607573@qq.com>
2022-04-15 17:13:03 +0800

    third commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("git test");
         System.out.println("hello git");
+        System.out.println("hell liuxin");
     }
 }
